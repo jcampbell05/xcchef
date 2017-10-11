0994235540c1db9b0627346fd7c548cc5f44711f
James Campbell <james@supmenow.com>
2017-10-11 07:54:49 +0100

    Added Guaka.

--- Sources/xcchef/main.swift
+++ Sources/xcchef/main.swift
@@ -1,1 +1,8 @@
 print("Hello, world!")
+
+// Port Functionality from cook
+// Download / Update template repositories
+// Collect from Xcode / Symlink
+// Mixins (if possible)
+// 
+// Xcode Template Generator
