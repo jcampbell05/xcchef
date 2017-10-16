54ea8d099ba9e9686829ef493b3e2f3d2efb2cdb
James Campbell <james@supmenow.com>
2017-10-16 07:33:50 +0100

    Removing regex.

--- Sources/template.swift
+++ Sources/template.swift
@@ -1,32 +1,23 @@
-import Foundation
 import PathKit
 
 class TemplateManager {
 
     let sharedTemplateLocation = Path("/Library/Developer/Xcode/Templates")
     let userTemplateLocation = Path("~/Library/Developer/Xcode/Templates")
-    let templateSourcePattern = try! NSRegularExpression(pattern: "([\\w\\.\\-\\_]+\\/[\\w\\.\\-\\_]+)(\\@[\\w\\.\\-\\_]+)*")
-    
+
     func install(templateSources: [String]) {
 
         for source in sourceComponents {
 
-            let matches = templateSourcePattern.matches(in: source, options: [], range: NSRange(location: 0, length: source.utf16.count))
+            let sourceComponents = source.components(separatedBy: "@")
+            let name = sourceComponents[0]
+            let version = sourceComponents.count > 1 ? sourceComponents[1] : "master"
+            let directory = name
 
-            if matches.count > 0  {
-                let sourceComponents = source.components(separatedBy: "@")
-                let name = sourceComponents[0]
-                let version = sourceComponents.count > 1 ? sourceComponents[1] : "master"
-                let directory = name
-
-                userTemplateLocation.shell("rm", "-rf", directory)
-                userTemplateLocation.shell("git", "clone", "-q", "https://github.com/" + name + ".git", directory, "-b", version)
-        
-                print("Installed \(name)")
-            
-            } else {
-                fatalError("Invalid template source provided")
-            }
+            userTemplateLocation.shell("rm", "-rf", directory)
+            userTemplateLocation.shell("git", "clone", "-q", "https://github.com/" + name + ".git", directory, "-b", version)
+    
+            print("Installed \(name)")
         }
     }
 
@@ -34,21 +25,14 @@ class TemplateManager {
 
         for source in sourceComponents {
 
-            let matches = templateSourcePattern.matches(in: source, options: [], range: NSRange(location: 0, length: source.utf16.count))
+            let sourceComponents = source.components(separatedBy: "@")
+            let name = sourceComponents[0]
+            let directory = name
+            let directoryPath = userTemplateLocation + Path(directory)
 
-            if matches.count > 0  {
-                let sourceComponents = source.components(separatedBy: "@")
-                let name = sourceComponents[0]
-                let directory = name
-                let directoryPath = userTemplateLocation + Path(directory)
+            directoryPath.shell("git", "pull")
 
-                directoryPath.shell("git", "pull")
-
-                print("Updated \(name)")
-            
-            } else {
-                fatalError("Invalid template source provided")
-            }
+            print("Updated \(name)")
         }
     }
 
@@ -56,20 +40,13 @@ class TemplateManager {
 
         for source in sourceComponents {
 
-            let matches = templateSourcePattern.matches(in: source, options: [], range: NSRange(location: 0, length: source.utf16.count))
+            let sourceComponents = source.components(separatedBy: "@")
+            let name = sourceComponents[0]
+            let directory = name
 
-            if matches.count > 0  {
-                let sourceComponents = source.components(separatedBy: "@")
-                let name = sourceComponents[0]
-                let directory = name
+            userTemplateLocation.shell("rm", "-rf", directory)
 
-                userTemplateLocation.shell("rm", "-rf", directory)
-
-                print("Uninstalled \(name)")
-            
-            } else {
-                fatalError("Invalid template source provided")
-            }
+            print("Uninstalled \(name)")
         }
     }
 }
